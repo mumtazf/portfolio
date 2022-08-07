d45c71b62f894633261d8533f508503c3e8a1c97
mumtazf <88557841+mumtazf@users.noreply.github.com>
2022-08-06 21:05:09 -0400

    Added the style.css and js files

---
+++ main.js
@@ -0,0 +1,65 @@
+
+
+function show(id){
+    document.getElementById(id).style.visibility = "visible";
+}
+
+function hide(id){
+    document.getElementById(id).style.visibility = "hidden";
+}
+
+function darkMode(){
+    var element = document.body;
+    element.classList.toggle("dark");
+    var dark_button = getElementById('dark_mode');
+    dark_button.innerHTML("Light Mode");
+}
+
+const checkbox = document.getElementById('dark_mode');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+  checkbox.innerText = "Light Mode";
+})
+
+function activate(){
+    var element = getElementById(id);
+    element.classList.toggle("active");
+}
+
+
+
+//     //this function is triggered when someone presses the boat 
+
+//     var boat_button = document.getElementById('boat_button');
+//     var welcome_form = document.getElementById('welcome_form');
+
+//     boat_button.addEventListener('click',() => {
+//         welcome_form.classList.toggle("show");
+
+// });
+
+
+
+
+
+//     const boat = document.getElementById('boat');
+
+//     boat.addEventListener('click',() => {
+//         const form = document.getElementById('welcome_form');
+//         if(form.style.display == 'none')
+//             form.style.display = 'block';
+//         else
+//             form.style.display = 'none';
+
+// })
+
+    // function boatPopup() {
+    //     document.getElementByID("boat").style.display = "block";
+    // }
+
+    // //This code is used to close the input form
+    // function closeForm(){
+    //     document.getElementById("myForm").style.display = "none";
+    // }
+ 
